01e30a3413b1e315456481aca5d3e8431aa77cb5
taddeimania <jtaddei@gmail.com>
2017-01-17 10:22:33 -0500

    Started OOP

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -6,10 +6,53 @@ using System.Threading.Tasks;
 
 namespace Day2
 {
+
+    // Class Definition
+    class Dog
+    {
+        private int Age;
+        public string name;
+
+        public Dog(string _name)
+        {
+            Age = 0;
+            name = _name;
+        }
+
+        public void HappyBirthday()
+        {
+            Age++;
+        }
+
+        public int GetAge()
+        {
+            return Age;
+        }
+
+        public int GetAgeDogYears()
+        {
+            return Age * 7;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("it works");
+            var peanut = new Dog("Peanut");
+            peanut.HappyBirthday();
+            peanut.HappyBirthday();
+            peanut.HappyBirthday();
+            peanut.HappyBirthday();
+            peanut.HappyBirthday();
+            var age = peanut.GetAge();
+            var ageDogYears = peanut.GetAgeDogYears();
+            Console.WriteLine(age);
+            Console.WriteLine(ageDogYears);
+            Console.WriteLine(peanut.name);
+            Console.ReadLine();
+
         }
     }
 }
